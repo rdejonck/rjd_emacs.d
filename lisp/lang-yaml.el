ab4785c9fb0cfa5a1fe93dedba08b6fd56c073f2
rjd <54892616+rdejonck@users.noreply.github.com>
2023-04-22 11:40:05 -0500

    Added the Ansible language server

--- lisp/lang-yaml.el
+++ lisp/lang-yaml.el
@@ -18,10 +18,22 @@
 ;;     $ npm install yaml-language-server@1.10.0
 (add-hook 'yaml-mode-hook 'eglot-ensure)
 
+;; Install the ansible language server
+;;     npm install @ansible/ansible-language-server@1.0.2
+;; Also install Ansible Lint and YAML Lint
+;;     $ pip3 install ansible-lint
+;;     $ apt install yamllint
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+	       `((yaml-ts-mode yaml-mode)
+		 . ,(eglot-alternatives '(("yaml-language-server" "--stdio")
+					  ("ansible-language-server" "--stdio"))))))
+
 ;;(add-hook 'yaml-mode-hook 'whitespace-mode)
 (add-hook 'yaml-mode-hook 'subword-mode)
 (add-hook 'yaml-mode-hook
 	  (lambda () (add-hook 'before-save-hook 'whitespace-cleanup nil t)))
+
 
 ;; Display indentation levels
 (require 'highlight-indent-guides)
